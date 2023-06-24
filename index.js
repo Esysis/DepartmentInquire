8568962110935bc0978e42aeeab9aa436d343f36
Yuga <Trev@brennberry.com>
2023-06-24 03:47:52 -0400

    Added javascript and inquirer code

---
+++ index.js
@@ -0,0 +1,59 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'password',
+    database: 'employee_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Exit'
+        ]
+    }).then((answer) => {
+        switch (answer.action) {
+            case 'View all departments':
+                viewAllDepartments();
+                break;
+            case 'View all roles':
+                viewAllRoles();
+                break;
+            case 'View all employees':
+                viewAllEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            default:
+                connection.end();
+                break;
+        }
+    });
+}
