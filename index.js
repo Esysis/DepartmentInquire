f7847736b4a1cd6374368576c253a064b9a9ea4d
Yuga <Trev@brennberry.com>
2023-09-07 18:31:16 -0400

    Redid incorrect index

--- index.js
+++ index.js
@@ -1,59 +1,288 @@
-const mysql = require('mysql2');
+// require dependencies
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const db = require('./db');
+require("console.table");
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'employee_db'
-});
+init();
 
-connection.connect((err) => {
-    if (err) throw err;
-    start();
-});
-
-function start() {
-    inquirer.prompt({
-        name: 'action',
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-            'Exit'
-        ]
-    }).then((answer) => {
-        switch (answer.action) {
-            case 'View all departments':
-                viewAllDepartments();
-                break;
-            case 'View all roles':
-                viewAllRoles();
-                break;
-            case 'View all employees':
-                viewAllEmployees();
-                break;
-            case 'Add a department':
-                addDepartment();
-                break;
-            case 'Add a role':
-                addRole();
-                break;
-            case 'Add an employee':
-                addEmployee();
-                break;
-            case 'Update an employee role':
-                updateEmployeeRole();
-                break;
-            default:
-                connection.end();
-                break;
+function init() {
+    mainPrompt();
+}
+// prompt user with the main menu
+async function mainPrompt() {
+    // prompt user for menu choice
+    const { choice } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "what would you like to do?",
+            choices: [
+                {
+                    name: "view employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                // {
+                //     name: "view employees by department",
+                //     value: "VIEW_EMPLOYEES_BY_DEPARTMENT"
+                // },
+                // {
+                //     name: "view employees by manager",
+                //     valeu: "VIEW_EMPLOYEES_BY_MANAGER"
+                // },
+                {
+                    name: "add employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                // {
+                //     name: "delete employee",
+                //     value: "DELETE_EMPLOYEE"
+                // },
+                {
+                    name: "update employee role",
+                    value: "UPDATE_EMPLOYEE_ROLE"
+                },
+                // {
+                //     name: "update employee manager",
+                //     value: "UPDATE_EMPLOYEE_MANAGER"
+                // },
+                {
+                    name: "view all roles",
+                    value: "VIEW_ROLES"
+                },
+                {
+                    name: "add role",
+                    value: "ADD_ROLE"
+                },
+                // {
+                //     name: "delete role",
+                //     value: "DELETE_ROLE"
+                // },
+                {
+                    name: "view all departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "quit?",
+                    value: "QUIT"
+                }
+            ]
         }
-    });
+    ]);
+    // route user to appropriate function based on choice
+    switch (choice) {
+        case "VIEW_EMPLOYEES":
+            return viewEmployees();
+        case "VIEW_EMPLOYEES_BY_DEPARTMENT":
+            return viewEmployeesByDepartment();
+        case "ADD_EMPLOYEE":
+            return addEmployee();
+        case "UPDATE_EMPLOYEE_ROLE":
+            return updateEmployeeRole();
+        case "VIEW_DEPARTMENTS":
+            return viewDepartments();
+        case "ADD_DEPARTMENT":
+            return addDepartment();
+        case "VIEW_ROLES":
+            return viewRoles();
+        case "ADD_ROLE":
+            return addRole();
+        default:
+            return quit();
+    }
+}
+// query database for all employees
+async function viewEmployees() {
+    const employees = await db.findEmployees();
+
+    console.table(employees);
+
+    mainPrompt();
+}
+// query the database for employees based on the departments
+async function viewEmployeesByDepartment() {
+    const departments = await db.findDepartments();
+
+    const userDepartmentChoice = departments.map(({ id, name }) => ({
+        name: name,
+        value: id
+    }));
+
+    const { departmentId } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "departmentId",
+            message: "which department would you like to choose?",
+            choices: userDepartmentChoice
+        }
+    ]);
+
+    const employees = await db.findEmployeesByDepartment(departmentId);
+
+    console.table(employees);
+
+    mainPrompt();
+}
+
+async function updateEmployeeRole() {
+    // get all the employees from the database
+    const employees = await db.findEmployees();
+    // map the employees to {name: full name, value: id} array for prompt choices
+    const userEmployeeChoice = employees.map(({ id, first_name, last_name }) => ({
+        name: first_name + ' ' + last_name,
+        value: id,
+    }));
+    // prompt user to select an employee
+    const { employeeId } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "employeeId",
+            message: "which employee's role would you like to update?",
+            choices: userEmployeeChoice
+        }
+    ]);
+    // get all roles from database
+    const roles = await db.findRoles();
+    // map roles to {name: title, value: id} array for prompt choices
+    const roleChoices = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+    // prompt user to select a role
+    const { roleId } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "roleId",
+            message: "which role do you want to assign to the employee?",
+            choices: roleChoices
+        }
+    ]);
+    // update employee role using database function
+    await db.updateEmployeeRole(employeeId, roleId);
+
+    console.log("successfully updated employee role!");
+
+    mainPrompt();
+}
+
+async function viewRoles() {
+    const roles = await db.findRoles();
+
+    console.table(roles);
+
+    mainPrompt();
+}
+
+async function addRole() {
+    const departments = await db.findDepartments();
+
+    const userDepartmentChoice = departments.map(({ id, name }) => ({
+        name: name,
+        value: id,
+    }));
+
+    const role = await inquirer.prompt([
+        {
+            name: "title",
+            message: "name of role?"
+        },
+        {
+            name: "salary",
+            message: "salary of the role?"
+        },
+        {
+            type: "list",
+            name: "department_id",
+            message: "which department does this role work for?",
+            choices: userDepartmentChoice
+        }
+    ]);
+
+    await db.createRole(role);
+
+    console.log(`${role.title} added to database.`);
+
+    mainPrompt();
+}
+
+async function viewDepartments() {
+    const departments = await db.findDepartments();
+
+    console.table(departments);
+
+    mainPrompt();
+}
+
+async function addDepartment() {
+    const department = await inquirer.prompt([
+        {
+            name: "name",
+            message: "what is the name of the department?"
+        }
+    ]);
+
+    await db.createDepartment(department);
+
+    console.log(`${department.name} added to the database.`);
+
+    mainPrompt();
+}
+
+async function addEmployee() {
+    const roles = await db.findRoles();
+    const employees = await db.findEmployees();
+
+    const employee = await inquirer.prompt([
+        {
+            name: "first_name",
+            message: "what is employee's first name?",
+        },
+        {
+            name: "last_name",
+        }
+    ]);
+
+    const userRoleChoice = roles.map(({ id, title }) => ({
+        name: title,
+        value: id
+    }));
+
+    const { roleId } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "roleId",
+            message: "what is the employee's role?",
+            choices: userRoleChoice
+        }
+    ]);
+
+    employee.role_id = roleId;
+
+    const userManagerChoices = employees.map(({ id, first_name, last_name }) => ({
+        name: first_name + ' ' + last_name,
+        value: id,
+    }));
+    userManagerChoices.unshift({ name: "none", value: null });
+
+    const { managerId } = await inquirer.prompt([
+        {
+            type: "list",
+            name: "managerId",
+            message: "who is the employee's manager?",
+            choices: userManagerChoices
+        }
+    ]);
+
+    employee.manager_id = managerId;
+
+    await db.createEmployee(employee);
+
+    console.log(`${employee.first_name} ${employee.last_name} added to the database.`);
+
+    mainPrompt();
+}
+
+function quit() {
+    console.log("adios~");
+    process.exit();
 }
